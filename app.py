8597bdaa32089d7e46caf86562b1f25a4601b8dc
epidarian <idownloadsyndrome@gmail.com>
2025-02-25 16:40:51 -0800

    fix app.py subprocess

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ from flask import Flask, request, render_template_string, url_for
 import io
 import base64
 import matplotlib.pyplot as plt
+import subprocess
+import os
+
+if not os.path.exists("probability_matrix_women_likes_men.csv"):
+    print("detected first run. Attempting to generate csv templates.")
+    subprocess.run(["python", "init.py"], check=True)
 
 # Import the simulation backend and relevant globals.
 from backend import run_tinder_simulation, all_men_ids, all_women_ids, all_user_ids
